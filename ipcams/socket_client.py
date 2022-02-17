971ffd98921ed29e0f2bcdfcac7a143749e10bf0
George <georgekorob@gmail.com>
2022-02-17 21:18:11 +0300

    sockets

---
+++ ipcams/socket_client.py
@@ -0,0 +1,32 @@
+import socket
+from pathlib import Path
+import sys
+import requests
+
+shotspath = Path(__file__).resolve().parent.parent / 'camerashots'
+namefile = shotspath / '001' / '00001.jpg'
+file = open(namefile, 'rb')
+
+ip = "localhost"
+port = 80
+
+# создаём сокет для подключения
+sock = socket.socket()
+sock.connect((ip, port))
+
+# имя файла отправляем серверу
+sock.send(namefile.name.encode('utf-8'))
+
+# открываем файл в режиме байтового чтения
+file_to_send = open(namefile, "rb")
+
+# читаем строку
+line = file_to_send.read(1024)
+
+while (line):
+    # отправляем строку на сервер
+    sock.send(line)
+    line = file_to_send.read(1024)
+
+file_to_send.close()
+sock.close()
---
+++ ipcams/socket_server.py
@@ -0,0 +1,41 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1', 80))
+server.listen(2)
+print("Working...")
+
+while True:
+    # начинаем принимать соединения
+    client_socket, address = server.accept()
+
+    # выводим информацию о подключении
+    print('Connected:', address)
+
+    # получаем название файла
+    namefile = client_socket.recv(1024).decode('utf-8')
+
+    # открываем файл в режиме байтовой записи
+    file_for_save = open(namefile, 'wb')
+
+    while True:
+
+        # получаем байтовые строки
+        line = client_socket.recv(1024)
+
+        # пишем байтовые строки в файл на сервере
+        file_for_save.write(line)
+
+        if not line:
+            break
+
+    # HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    # content = 'Well done, buddy!'.encode('utf-8')
+    # client_socket.send(HDRS.encode('utf-8') + content)
+    file_for_save.close()
+    client_socket.close()
+
+    print('File received')
+
+print('Shutdown!')
+server.close()
